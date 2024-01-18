e21559444217f4f27e8b1610221f1c7b344e332d
David Ferreira <ferreiradavid361@gmail.com>
2024-01-18 12:12:25 -0300

    continuando a criação da api

--- server/main.js
+++ server/main.js
@@ -1,7 +1,16 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import routes from './routes';
+
+dotenv.config({ path: '../.env' });
 
 const app = express();
-app.use(cors());
+const port = process.env.PORT || 3001;
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', routes);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
