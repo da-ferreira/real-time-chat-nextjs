1176904ca37c61d9c2b40d6e159f5f52e2b17ae8
David Ferreira <ferreiradavid361@gmail.com>
2024-01-21 12:55:49 -0300

    criando demais metodos de usuarios

--- server/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { create, login } from '../controllers/user.controller';
-
-const router = Router();
-
-router.post('/create', create);
-router.post('/login', login);
-
-export default router;
---
+++ server/src/models/user.model.js
@@ -0,0 +1,34 @@
+import { prisma } from '../config/prisma.client.js';
+import bcrypt from 'bcrypt';
+
+export default {
+  async create(name, email, password) {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    return prisma.user.create({
+      data: { name, email, password: hash },
+    });
+  },
+
+  async findByField(field, value) {
+    return prisma.user.findUnique({
+      where: { [field]: value },
+      select: { id: true, name: true, email: true, createdAt: true, updatedAt: true },
+    });
+  },
+
+  async isValidPassword(password, hash) {
+    return bcrypt.compareSync(password, hash);
+  },
+
+  async findAll(email, name) {
+    return prisma.user.findMany({
+      where: {
+        email: { contains: email || '' },
+        name: { contains: name || '' },
+      },
+      select: { id: true, name: true, email: true, createdAt: true, updatedAt: true },
+    });
+  },
+};
